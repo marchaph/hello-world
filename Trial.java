54473de2614c015ddad178fa42aeaf97c95dba12
marchaph <marchaph@lovelandschools.org>
2021-08-03 13:51:30 -0400

    Update Trial.java

--- Trial.java
+++ Trial.java
@@ -5,5 +5,6 @@ public class Trial
     System.out.println("Hello world.");
     System.out.println("Today is Tuesday.");
     System.out.println("Tomorrow is Wednesday!");
+    System.out.print("End of the week.");
   }
 }
